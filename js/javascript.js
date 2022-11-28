bae0fd2454d4096fd43aa0b05ae83bee48e7b3f5
antonelaaguirre <115751532+antonelaaguirre@users.noreply.github.com>
2022-11-28 11:58:15 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+function inclusive()
+    {
+        document.body.style.backgroundColor= "black";
+        document.getElementById("header").style.backgroundColor = "black";
+        document.getElementById("header").style.color = "#90ffe9";
+        document.getElementById("nav").style.backgroundColor = "whitesmoke";
+        document.getElementById("divredes").style.backgroundColor="whitesmoke";
+        document.getElementById("nav").style.color = "black";
+        document.getElementById("section").style.backgroundColor = "black";
+        document.getElementById("section").style.color = "whitesmoke";
+        document.getElementById("aside").style.backgroundColor = "black";
+        document.getElementById("footer").style.backgroundColor = "black";
+        document.getElementById("redesfoot").style.backgroundColor="whitesmoke";
+    }
+
+    function copy() {
+    alert("Autora: Aguirre Antonela Belén. EMAIL: antoaguirre350@gmail.com");
+    }
